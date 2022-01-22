44b802b81c0c563cd92595c0fe98d995b87d776c
santosh-07shrestha <santosh.07shrestha@gmail.com>
2022-01-22 14:35:16 -0500

    odd even function

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,11 @@
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Assignment");
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter a number to check odd or even: ");
+		OddEven.OddEvenFun(scan.nextInt());
 	}
 
 }
---
+++ src/OddEven.java
@@ -0,0 +1,9 @@
+
+public class OddEven {
+	public static void OddEvenFun(int num) {
+		if(num%2==0)
+			System.out.println("Even Number");
+		else
+			System.out.println("Odd Number");
+	}
+}
